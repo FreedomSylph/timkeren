beadc6119f05e99fed50b4255fa2da4ddcd50b64
FreedomSylph <yuri_raphaelus@yahoo.co.id>
2015-11-25 15:08:29 +0700

    Update Task 1 & 2
    Task 1 belum implement per 5 wave, penambahan variable dalam coin.cs
    untuk memisahkan currency reserve (yang player pegang) dengan currency
    temp (yang didapat dalam stage).
    Task 2 entah mengapa, sudah di set agar pop 25, tetapi in-game masih
    hanya 5. Sudah ada fungsi dalam Coin.cs agar generate income per 2s
    sesuai dengan jumlah pop.

--- Valiant Guardian - Demon Invasion/Assets/Scripts/Base/PlayerBase.cs
+++ Valiant Guardian - Demon Invasion/Assets/Scripts/Base/PlayerBase.cs
@@ -4,11 +4,13 @@ using UnityEngine.UI;
 
 public class PlayerBase : MonoBehaviour
 {
+    private int baseLevel;
     private Text enemyLimitText;
-    public int enemyLimit = 5;
+    public int enemyLimit = 25;
 
     void Awake()
     {
+        baseLevel = 1;
         enemyLimitText = GameObject.Find("EnemyLimitText").GetComponent<Text>();
         enemyLimitText.text = enemyLimit.ToString();
     }
@@ -34,5 +36,26 @@ public class PlayerBase : MonoBehaviour
         enemyLimitText = GameObject.Find("EnemyLimitText").GetComponent<Text>();
         enemyLimitText.text = "You Wiiiin";
         Time.timeScale = 0;
+    }
+
+    public void Upgrade()
+    {
+        baseLevel++;
+        if (baseLevel == 2)
+        {
+            enemyLimit = 30;
+        }
+        if (baseLevel == 3)
+        {
+            enemyLimit = 35;
+        }
+        if (baseLevel == 4)
+        {
+            enemyLimit = 40;
+        }
+        if (baseLevel == 5)
+        {
+            enemyLimit = 50;
+        }
     }
 }
--- Valiant Guardian - Demon Invasion/Assets/Scripts/Stage_UI/Coin.cs
+++ Valiant Guardian - Demon Invasion/Assets/Scripts/Stage_UI/Coin.cs
@@ -5,25 +5,75 @@ using UnityEngine.UI;
 public class Coin : MonoBehaviour
 {
 
-    private int countCoin = 0;
-    private Text coinText;
+    private int countCoin = 0; //reserve
+    private int countTempCoin = 0;
+    private int income = 0;
+    private Text coinText; //tampilkan reserve + temp
+    private PlayerBase playerBase;
 
     void Awake()
     {
         coinText = transform.FindChild("CoinText").GetComponent<Text>();
         //Always load coin from dataPlayer
         countCoin = DataPlayer.getInstance().Coin;
-        coinText.text = countCoin.ToString();
+        //coinText.text = countCoin.ToString();
+        coinText.text = (countCoin + countTempCoin).ToString();
+        playerBase = GameObject.Find("PlayerBase").GetComponent<PlayerBase>();
+    }
+
+    public void resetTempCoin()
+    {
+        countTempCoin = 0;
     }
 
     public void addCoin()
     {
-        countCoin += 10;
+        countTempCoin += 10;
         //set coin data in DataPlayer
-        DataPlayer.getInstance().Coin = countCoin;
-        coinText.text = countCoin.ToString();
+        //DataPlayer.getInstance().Coin = countCoin;
+        //coinText.text = countCoin.ToString();
+        coinText.text = (countCoin + countTempCoin).ToString();
         //always save coin data for every time get coin
         //todo in Future will be improved, because can (maybe) be a killer performance
         //because every state coin will access disk to write file
     }
+
+    public void addToReserve()
+    {
+        countCoin = countTempCoin + countCoin;
+        //DataPlayer.getInstance().Coin = countCoin;
+    }
+
+    public void subtractCoin(int amount)
+    {
+        countTempCoin -= amount;
+        if (countTempCoin < 0)
+        {
+            countCoin += countTempCoin;
+            resetTempCoin();
+        }
+    }
+
+    public void incomeRegen()
+    {
+        CancelInvoke();
+        if (playerBase.enemyLimit > 40)
+            income = 30;
+        else if (playerBase.enemyLimit > 30)
+            income = 25;
+        else if (playerBase.enemyLimit > 20)
+            income = 20;
+        else if (playerBase.enemyLimit > 10)
+            income = 15;
+        else
+            income = 10;
+        InvokeRepeating("regenAdd", 2f, 2f);
+    }
+
+    public void regenAdd()
+    {
+        countTempCoin += income;
+        coinText.text = (countCoin + countTempCoin).ToString();
+    }
+
 }
